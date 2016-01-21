ed8c1067eb5104f307acef2e5271cab5a7e3227f
Thor Amorim <thoramorim@gmail.com>
2016-01-21 15:24:33 -0200

    Change image res from 80x80 to 320x320

--- src/components/ProductList/ListLayout/ListProduct/ListProduct.js
+++ src/components/ProductList/ListLayout/ListProduct/ListProduct.js
@@ -8,7 +8,7 @@ const Price = stores.ComponentStore.state.getIn(['Price@vtex.storefront-sdk', 'c
 class ListProduct extends React.Component {
   render() {
     let defaultSku = this.props.skus[0];
-    let imageUrl = defaultSku.images[0].src.replace(/(#width#|#height#)/g, '80');
+    let imageUrl = defaultSku.images[0].src.replace(/(#width#|#height#)/g, '320');
     let listPrice = defaultSku.offers[0].listPrice;
     let price = defaultSku.offers[0].price;
 
