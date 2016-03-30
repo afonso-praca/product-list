bc08041377c4b47a2d36130b0a3e29ded8d614cd
afonso <afonsoinfo@gmail.com>
2016-03-30 10:54:57 -0300

    style on product list

--- src/components/ProductList/ListLayout/ListProduct/ListProduct.js
+++ src/components/ProductList/ListLayout/ListProduct/ListProduct.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import '../../theme-variables.less';
 import './ListProduct.less';
+import './ListProductCustom.less';
 import { stores } from 'sdk';
 
 const Link = stores.ComponentStore.state.getIn(['Link@vtex.storefront-sdk', 'constructor']);
@@ -16,26 +18,24 @@ class ListProduct extends React.Component {
       <div className="ListProduct__parent">
         <div className="ListProduct">
           <Link to={`/${this.props.slug}/p`}>
-            <div className="ListProduct__image-wrapper col-xs-4 col-sm-4 col-md-3 col-lg-3">
+            <div className="ListProduct__image-wrapper col-xs-6 col-sm-4 col-md-3 col-lg-3">
               <img className="ListProduct__image" src={imageUrl} />
             </div>
           </Link>
-          <div className="ListProduct__content col-xs-8 col-sm-8 col-md-9 col-lg-9">
+          <div className="ListProduct__content col-xs-6 col-sm-8 col-md-9 col-lg-9">
             <div>
-              <h4 className="ListProduct__name theme__font-family--main">
-                <Link to={`/${this.props.slug}/p`} className="theme__color--dark theme__hover-color--dark">
+              <h4 className="ListProduct__name">
+                <Link to={`/${this.props.slug}/p`} className="">
                   { this.props.name }
                 </Link>
               </h4>
-              <div className="ListProduct__price-from theme__color--dark">
-                <span>de </span>
+              <div className="ListProduct__price-from">
                 <span className="ListProduct__price-strike">
                   <Price value={listPrice} />
                 </span>
               </div>
               <Link className="ListProduct__price-by" to={`/${this.props.slug}/p`}>
-                <div className="theme__color--accent theme__hover-color--accent">
-                  <span>por </span>
+                <div className="">
                   <span className="ListProduct__price">
                     <Price value={price} />
                   </span>
@@ -43,11 +43,7 @@ class ListProduct extends React.Component {
               </Link>
             </div>
             <Link to={`/${this.props.slug}/p`}>
-              <button className="
-                ListProduct__button
-                theme__background-color--secondary
-                theme__background-hover-color--secondary
-                theme__font-family--main">
+              <button className="ListProduct__button">
                 Ver Mais
               </button>
             </Link>
