5a7ccd840791e488196cc0be7b1c6560b0f3a150
Anderson Moreira <anderson.moreira@vtex.com.br>
2016-04-30 16:07:07 -0300

    Hofix: Remove loadash dependency

--- src/components/ProductList/GridLayout/GridLayout.js
+++ src/components/ProductList/GridLayout/GridLayout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import GridProduct from './GridProduct/GridProduct';
-import map from 'lodash-compat/collection/map';
 import './GridLayout.less';
 import './GridLayoutCustom.less';
 
 class GridLayout extends React.Component {
   render() {
     let products = this.props.products ?
-      map(this.props.products, function(product) {
+      this.props.products.map((product) => {
         return (
           <div className="GridLayout__clear" key={product.slug}>
             <div className="GridLayout col-xs-6 col-sm-4 col-md-4 col-lg-4">
--- src/components/ProductList/ListLayout/ListLayout.js
+++ src/components/ProductList/ListLayout/ListLayout.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ListProduct from './ListProduct/ListProduct';
-import map from 'lodash-compat/collection/map';
 
 class ListLayout extends React.Component {
   render() {
     let products = this.props.products ?
-      map(this.props.products, function(product) {
+      this.props.products.map((product) => {
         return (
           <div key={product.slug} className="ListLayout__product">
             <ListProduct {...product} />
           </div>
         );
-      }) :
-      ( <div>Carregando</div> );
-
+      }) :( <div>Carregando</div> );
+      
     return (
       <div className="ListLayout">
         { products }
